947aa3b84ab0e3e75d504fc7000d8ab9366af709
Darek Wieliczko <darek@dar-soft.com.pl>
2024-05-05 11:56:43 +0200

    Init

---
+++ DWReact1/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+const element = <h1>Hello, world!</h1>;
+
+ReactDOM.createRoot(document.getElementById('root')).render(element);
+
